94e87a15f40f7fc07f062116908d8d4860e9c738
aidatuhcic <Aida.Tuhcic@flemingcollege.ca>
2022-04-09 23:23:54 -0400

    JavaScript added and commented.

---
+++ JS/javascript.js
@@ -0,0 +1,9 @@
+// source: https://www.w3schools.com/howto/howto_js_responsive_navbar_dropdown.asp
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
