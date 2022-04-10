8652b71297dfcfb0d6160f11c5e9f247e261114e
aidatuhcic <Aida.Tuhcic@flemingcollege.ca>
2022-04-10 03:54:54 -0400

    JavaScript edited and commented.

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,4 +1,5 @@
-// source: https://www.w3schools.com/howto/howto_js_responsive_navbar_dropdown.asp
+// source of the code: https://www.w3schools.com/howto/howto_js_responsive_navbar_dropdown.asp
+// the javascript will call on the navigation from the html, in order to ensure it resizes for smaller screen devices.  
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
