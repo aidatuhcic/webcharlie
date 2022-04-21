df5ceb43be2fed4e27a5c0a40d3cfcdb9153d6ce
SunaSashi <sunayana.sashikumar@flemingcollege.ca>
2022-04-21 12:45:46 -0400

    Update data2018.js

--- JS/data2018.js
+++ JS/data2018.js
@@ -1,16 +1,16 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3VuYXNhc2hpIiwiYSI6ImNsMXhvNnpmeTA0b2EzY281NHd3eWp2bm8ifQ.FJ6hyKEqn4EnsT69WRMVsA';
 const map = new mapboxgl.Map({
-container: 'map', // container ID
+container: 'map2', // container ID
 style: 'mapbox://styles/mapbox/satellite-v9', // style URL
 zoom: 7, // starting zoom
-center: [138.043, 35.201] // starting center
+center: [43.6532, -79.3832] // starting center
 });
  
 map.on('load', () => {
 map.addSource('earthquakes', {
 type: 'geojson',
 // Use a URL for the value for the `data` property.
-data: 'data2019.geojson'
+data: 'data2018.geojson'
 });
  
 map.addLayer({
