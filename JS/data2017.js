d04d4f7d458089b72e670a8379ffee688e5557e4
SunaSashi <sunayana.sashikumar@flemingcollege.ca>
2022-04-21 12:42:11 -0400

    Create data2017.js

---
+++ JS/data2017.js
@@ -0,0 +1,27 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3VuYXNhc2hpIiwiYSI6ImNsMXhvNnpmeTA0b2EzY281NHd3eWp2bm8ifQ.FJ6hyKEqn4EnsT69WRMVsA';
+const map = new mapboxgl.Map({
+container: 'map', // container ID
+style: 'mapbox://styles/mapbox/satellite-v9', // style URL
+zoom: 7, // starting zoom
+center: [138.043, 35.201] // starting center
+});
+ 
+map.on('load', () => {
+map.addSource('earthquakes', {
+type: 'geojson',
+// Use a URL for the value for the `data` property.
+data: 'data2017.geojson'
+});
+ 
+map.addLayer({
+'id': 'earthquakes-layer',
+'type': 'circle',
+'source': 'earthquakes',
+'paint': {
+'circle-radius': 8,
+'circle-stroke-width': 2,
+'circle-color': 'red',
+'circle-stroke-color': 'white'
+}
+});
+});
